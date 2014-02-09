5f82356192025d8867a025706e19c495493e6a39
David J. Christy <davidcis@gmail.com>
2014-02-09 12:15:29 -0800

    Created full_title function

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+	let(:base_title){"Ruby on Rails Tutorial Sample App"}
 	describe "Home page" do
 		it "should have the content 'Sample App'" do
 			visit '/static_pages/home'
 			expect(page).to have_content('Sample App')
 		end
 
-		it "should have the title 'Home'" do
+		it "should not have the title 'Home'" do
 			visit "/static_pages/home"
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+			expect(page).not_to have_title("#{base_title} | Home")
 		end
 
 	end
@@ -22,7 +24,7 @@ describe "Static pages" do
 
 		it "should have the title 'Help'" do
 			visit "/static_pages/help"
-			expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+			expect(page).to have_title("#{base_title} | Help")
 		end		
 	end
 
